5ad5dac010bec4a5c732a86d7397125fcda8f2d7
Sebastian Feuerstack <Sebastian@Feuerstack.de>
2010-10-27 15:18:22 -0200

    dfasdfasdfadfa

--- scxml.rb
+++ scxml.rb
@@ -6,7 +6,7 @@ module Statemachine
     # this function  extends the statemachine module lib/statemachine/builder.rb
     # here we need to start adding code to add scxml parsing to the statemachine gem
     p "Hello world!"
-    
+    # Test Test
     return statemachine
   end
 end

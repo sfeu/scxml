25be462f192c3a074347e4344c598408eb2aa0a6
jessica <jessicacolnago@hotmail.com>
2011-06-10 18:08:15 -0300

    creation of scxml and added history to parser

---
+++ statemachines/AIO_scxml_spec.rb
@@ -0,0 +1,36 @@
+require File.dirname(__FILE__) + '/spec_helper'
+
+describe "AIO" do
+  before (:each) do
+    parser = StatemachineParser.new
+    @statemachine = parser.build_from_scxml "../aui-scxml/AIO.scxml"
+   end
+
+
+  it "should start in :initialized" do
+      @statemachine.state.should == :initialized
+  end
+
+  it "should enter the superstate :presenting" do
+      @statemachine.organize
+      @statemachine.state.should == :organized
+      @statemachine.present
+      @statemachine.state.should == :defocused
+  end
+
+  it "should support transitions inside the superstate" do
+      @statemachine.organize
+      @statemachine.present
+      @statemachine.focus
+      @statemachine.state.should == :focused
+      @statemachine.defocus
+      @statemachine.state.should == :defocused
+  end
+
+  it "should leave the superstate" do
+      @statemachine.organize
+      @statemachine.present
+      @statemachine.suspend
+      @statemachine.state.should == :suspended
+  end
+end

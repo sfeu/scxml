faed83e5b9d35d0b387446dacb34eae8250084c5
jessica <jessicacolnago@hotmail.com>
2011-08-03 14:54:35 -0300

    foo

---
+++ scxml.rb
@@ -0,0 +1,12 @@
+
+module Statemachine
+  def self.build_from_scxml(filename)
+    statemachine = nil
+    
+    # this function  extends the statemachine module lib/statemachine/builder.rb
+    # here we need to start adding code to add scxml parsing to the statemachine gem
+    p "Hello world!"
+    # Test Test
+    return statemachine
+  end
+end
